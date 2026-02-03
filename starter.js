5186a2052ace7ff8efbb41ec85513e310881ca65
pumkinrino <quangminhvu1314@gmail.com>
2026-02-03 20:25:37 +0700

    dùng module trung gian để gọi chatbot popup trên web thay vì thêm trực tiếp

--- starter.js
+++ starter.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-script.src = "https://lms-bot-441o.onrender.com/chatbot.js";
+script.src = "https://lms-bot-441o.onrender.com/chatbot.js"; 
 document.head.appendChild(script);
